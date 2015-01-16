223023440b7b0df2acfcea1a6bda512a7adaf5b2
Ewen Cheslack-Postava <me@ewencp.org>
2015-01-16 11:11:54 -0800

    Make AbstractPerformanceTest support dynamic rate control.
    Provide methods in AbstractPerformanceTest that can be overridden to control the
    rate limiting. This is useful if the target rate the performance test wants to
    hit cannot be expressed as a constant rate of iterations per second, e.g. if it
    actually wants messages/s or bytes/s and each iteration has a variable number of
    messages or bytes.

--- utils/src/main/java/io/confluent/common/utils/AbstractPerformanceTest.java
+++ utils/src/main/java/io/confluent/common/utils/AbstractPerformanceTest.java
@@ -20,8 +20,11 @@ public abstract class AbstractPerformanceTest {
   private static final long NS_PER_MS = 1000000L;
   private static final long NS_PER_SEC = 1000 * NS_PER_MS;
   private static final long MIN_SLEEP_NS = 2 * NS_PER_MS;
+  private static final long TARGET_RATE_UPDATE_PERIOD_MS = 100;
 
   protected PerformanceStats stats;
+
+  protected long rate = 0;
 
   public AbstractPerformanceTest(long numEvents, int reportingInterval) {
     stats = new PerformanceStats(numEvents, reportingInterval);
@@ -47,23 +50,51 @@ public abstract class AbstractPerformanceTest {
    */
   protected abstract boolean runningFast(int iteration, float elapsed);
 
-  protected void run(long iterationsPerSec) throws InterruptedException {
-    long sleepTime = NS_PER_SEC / iterationsPerSec;
+  /**
+   * Returns the target number of iterations that should be executed per second. A value of 0
+   * indicates no rate limiting should be applied. This method will be executed periodically to
+   * allow the rate to adjust when tests want a value other than iteration rate to remain
+   * constant, e.g. throughput.
+   * @return the target number of iterations, or 0 if there should be no limit
+   */
+  protected float targetRate(int iteration, float elapsed) {
+    return rate;
+  }
+
+  /**
+   * Run the performance test, trying to achieve the target rate returned by {@link #targetRate
+   * (int, float)}.
+   * @throws InterruptedException
+   */
+  protected void run() throws InterruptedException {
     long sleepDeficitNs = 0;
     long start = System.currentTimeMillis();
+    long targetRateUpdated = start;
+    float currentTargetRate = targetRate(0, 0);
+    long sleepTime = (long)(NS_PER_SEC / currentTargetRate);
     for (int i = 0; !finished(i); i++) {
       long sendStart = System.currentTimeMillis();
-      PerformanceStats.Callback cb = stats.nextCompletion(sendStart);
-      doIteration(cb);
 
       /*
        * Maybe sleep a little to control throughput. Sleep time can be a bit inaccurate for
        * times < 1 ms so instead of sleeping each time instead wait until a minimum sleep time
        * accumulates (the "sleep deficit") and then make up the whole deficit in one longer sleep.
+       *
+       * The ordering of this code and the call to doIteration is the opposite of what you
+       * might expect because we need to be able to respond immediately to quick changes in
+       * target rate at the very start of the test when the user overrides #targetRate(), and
+       * be careful about the iteration # and elapsed time parameters passed to #targetRate()
+       * and #runningFast(). The code is simpler/requires fewer calls to System#currentTimeMillis
+       * in this order.
        */
-      if (iterationsPerSec > 0) {
+      if (currentTargetRate > 0) {
         float elapsed = (sendStart - start) / 1000.f;
-        if (elapsed > 0 && runningFast(i, elapsed)) {
+        if (i == 1 || (i > 1 && sendStart - targetRateUpdated > TARGET_RATE_UPDATE_PERIOD_MS)) {
+          currentTargetRate = targetRate(i, elapsed);
+          sleepTime = (long)(NS_PER_SEC / currentTargetRate);
+          targetRateUpdated = sendStart;
+        }
+        if (i > 0 && elapsed > 0 && runningFast(i, elapsed)) {
           sleepDeficitNs += sleepTime;
           if (sleepDeficitNs >= MIN_SLEEP_NS) {
             long sleepMs = sleepDeficitNs / 1000000;
@@ -73,9 +104,23 @@ public abstract class AbstractPerformanceTest {
           }
         }
       }
+
+      PerformanceStats.Callback cb = stats.nextCompletion(sendStart);
+      doIteration(cb);
     }
 
     /* print final results */
     stats.printTotal();
+  }
+
+  /**
+   * Run the performance test and try to hit a fixed target rate. If you use this method, you
+   * should not override the {@link #targetRate(int, float)} method.
+   * @param iterationsPerSec target number of iterations per second
+   * @throws InterruptedException
+   */
+  protected void run(long iterationsPerSec) throws InterruptedException {
+    rate = iterationsPerSec;
+    run();
   }
 }
