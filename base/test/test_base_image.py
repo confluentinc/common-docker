2c6e2c60d251fd85e6f395d6d9758474900e4ace
Eli Smaga <eli@confluent.io>
2019-12-04 08:14:17 -0800

    ST-2560: Adding rhel8 base image

--- base/test/test_base_image.py
+++ base/test/test_base_image.py
@@ -12,12 +12,6 @@ class BaseImageTest(unittest.TestCase):
     def test_image_build(self):
         self.assertTrue(utils.image_exists(self.image))
 
-    def test_java_install(self):
-        cmd = "java -version"
-        expected = 'OpenJDK Runtime Environment (Zulu 8.'
-        output = utils.run_docker_command(image=self.image, command=cmd)
-        self.assertTrue(expected in output)
-
     def test_dub_exists(self):
         self.assertTrue(utils.path_exists_in_image(self.image, "/usr/local/bin/dub"))
         self.assertTrue(utils.path_exists_in_image(self.image, "/usr/local/bin/cub"))
