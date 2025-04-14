1952e72a147e64bc4a980cca9023982b2052f0f9
Prince Raheja <114437476+rahejaprince@users.noreply.github.com>
2025-04-14 19:21:12 +0530

    CPBR-2474: log4j2 migration (#715)

--- utility-belt/src/main/java/io/confluent/admin/utils/cli/KafkaReadyCommand.java
+++ utility-belt/src/main/java/io/confluent/admin/utils/cli/KafkaReadyCommand.java
@@ -24,8 +24,8 @@ import net.sourceforge.argparse4j.inf.Namespace;
 
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.common.utils.Utils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -50,7 +50,7 @@ import static net.sourceforge.argparse4j.impl.Arguments.store;
  */
 public class KafkaReadyCommand {
 
-  private static final Logger log = LoggerFactory.getLogger(KafkaReadyCommand.class);
+  private static final Logger log = LogManager.getLogger(KafkaReadyCommand.class);
   public static final String KAFKA_READY = "kafka-ready";
 
   private static ArgumentParser createArgsParser() {
